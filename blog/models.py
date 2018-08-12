ab99c352aea555c892e80f1313ab3f342c5d3ed3
Abliman <ick@touchcoreltd.com>
2018-08-12 17:31:26 +0100

    Bootstrap Up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+
+    Title = models.CharField(max_length=50)
+    Date = models.DateTimeField(auto_now_add=False)
+    Body = models.TextField()
+    Image = models.ImageField(upload_to='images/')
